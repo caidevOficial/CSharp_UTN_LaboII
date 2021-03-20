cd7b08a64d18c99ba0757da5677888bfd57a4d91
CaidevOficial <falcone.facu@gmail.com>
2021-03-20 00:21:31 -0300

    Ejercicio - 05

---
+++ Ejercicio_05/Ejercicio_05/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NC = NumericCenter;
+
+namespace Ejercicio_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NC.NumericCenter.CheckNumericCenter();
+        }
+    }
+}
---
+++ Ejercicio_05/NumericCenter/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericCenter
+{
+    public class NumericCenter
+    {
+        /// <summary>
+        /// Searchs for numeric centers till' the number that the user enters.
+        /// </summary>
+        public static void CheckNumericCenter()
+        {
+            Console.Title = "Ejercicio Nro 05";
+
+            int numberTillCheck;
+            int sumOfAbove;
+            int sumOfFollowing;
+            int sumTotal;
+            bool flag = false;
+
+            Console.Write("Escriba un numero: ");
+            int.TryParse(Console.ReadLine(), out numberTillCheck);
+
+            Console.WriteLine("Buscando centros numéricos:\n");
+
+            for (int i = 2; i < numberTillCheck; i++)
+            {
+                sumOfFollowing = i + 1;
+                sumOfAbove = 0;
+                sumTotal = 0;
+
+                for (int y = i - 1; y > 0; y--)
+                {
+                    sumOfAbove += y;
+                }
+
+                while (sumTotal <= sumOfAbove)
+                {
+
+                    sumTotal += sumOfFollowing;
+                    if (sumTotal == sumOfAbove)
+                    {
+                        Console.WriteLine($"Centro encontrado: {i}");
+                        flag = true;
+
+                    }
+                    sumOfFollowing += 1;
+
+                }
+            }
+            if (!flag)
+            {
+                Console.WriteLine("Sin centros numéricos");
+            }
+            Console.WriteLine("\nProgram Terminated!");
+            Console.ReadKey(true);
+        }
+    }
+}
