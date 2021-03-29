d3958899aa9b0606b00679ee57256dcb6bdc5916
[FacuFalcone] <falcone.facu@gmail.com>
2021-03-29 20:55:26 -0300

    Practica Windows Forms - Mini Super

---
+++ Practica_WF_MiniSuper/Practica_WF_MiniSuper/FrmMain.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Practica_WF_MiniSuper
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.btnNuevoCliente = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.ForeColor = System.Drawing.Color.Red;
+            this.lblDireccion.Location = new System.Drawing.Point(13, 13);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(161, 13);
+            this.lblDireccion.TabIndex = 0;
+            this.lblDireccion.Text = "Bienvenidos al Mini Super de C#";
+            // 
+            // btnNuevoCliente
+            // 
+            this.btnNuevoCliente.BackColor = System.Drawing.SystemColors.MenuText;
+            this.btnNuevoCliente.ForeColor = System.Drawing.Color.Red;
+            this.btnNuevoCliente.Location = new System.Drawing.Point(16, 54);
+            this.btnNuevoCliente.Name = "btnNuevoCliente";
+            this.btnNuevoCliente.Size = new System.Drawing.Size(158, 35);
+            this.btnNuevoCliente.TabIndex = 1;
+            this.btnNuevoCliente.Text = "Nuevo Cliente";
+            this.btnNuevoCliente.UseVisualStyleBackColor = false;
+            this.btnNuevoCliente.MouseLeave += new System.EventHandler(this.btnNuevoCliente_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.ClientSize = new System.Drawing.Size(200, 104);
+            this.Controls.Add(this.btnNuevoCliente);
+            this.Controls.Add(this.lblDireccion);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mini Super";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Button btnNuevoCliente;
+    }
+}
+
