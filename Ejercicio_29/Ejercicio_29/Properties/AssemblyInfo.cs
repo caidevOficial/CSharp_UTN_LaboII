5b6d8958d926138cdcffb58794f508650145ca6b
[FacuFalcone] <falcone.facu@gmail.com>
2021-04-06 18:49:09 -0300

    Ejercicio - 29 [Base]

--- Ejercicio_29/Ejercicio_29/Properties/AssemblyInfo.cs
+++ Ejercicio_29/Ejercicio_29/Properties/AssemblyInfo.cs
@@ -1,9 +1,10 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
-// asociada con un ensamblado.
+// asociada a un ensamblado.
 [assembly: AssemblyTitle("Ejercicio_29")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("0ab158b6-36da-4b9b-890e-ca6953714f55")]
+[assembly: Guid("26d34695-f024-4db8-b7e8-2a52cd3f694d")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
