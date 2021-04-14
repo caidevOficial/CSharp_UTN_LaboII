75543e981a2cd03faa93900430ec0aa0fb834c2a
[FacuFalcone] <falcone.facu@gmail.com>
2021-04-13 22:49:19 -0300

    Ejercicio - 39 [Terminado]

---
+++ Ejercicios_Guia/Ejercicio_39/Entities/SobreSobreescrito.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * MIT License
+ * 
+ * Copyright (c) 2021 [FacuFalcone]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace Entities
+{
+    public class SobreSobreescrito : Sobreescrito
+    {
+        /// <summary>
+        /// Builder of the entity.
+        /// </summary>
+        public SobreSobreescrito() : base() { }
+
+        /// <summary>
+        /// Override implementation of MiPropiedad.
+        /// </summary>
+        public override string MiPropiedad { get => base.miAtributo; }
+
+        /// <summary>
+        /// Override implementation of MiMetodo.
+        /// </summary>
+        /// <returns>The info of MiPropiedad as a string.</returns>
+        public override string MiMetodo()
+        {
+            return MiPropiedad;
+        }
+    }
+}
