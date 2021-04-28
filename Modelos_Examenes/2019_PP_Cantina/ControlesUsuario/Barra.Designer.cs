1fe1b739d1f8aeeef516305525b497f68deaf027
[FacuFalcone] <falcone.facu@gmail.com>
2021-04-28 01:37:37 -0300

    2019_PP_Cantina [Terminado]

---
+++ Modelos_Examenes/2019_PP_Cantina/ControlesUsuario/Barra.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace ControlCantina
+{
+    partial class Barra
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Barra));
+            this.SuspendLayout();
+            // 
+            // Barra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.Name = "Barra";
+            this.Size = new System.Drawing.Size(551, 368);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
