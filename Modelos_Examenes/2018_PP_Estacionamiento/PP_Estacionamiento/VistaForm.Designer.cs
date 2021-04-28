a755bf620f288de3fcf5b907431557422a7e3516
[FacuFalcone] <falcone.facu@gmail.com>
2021-04-28 02:50:01 -0300

    2018_PP_Estacionamiento

---
+++ Modelos_Examenes/2018_PP_Estacionamiento/PP_Estacionamiento/VistaForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace PP_Estacionamiento
+{
+    partial class frmPickUp
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPickUp));
+            this.lblModelo = new System.Windows.Forms.Label();
+            this.lblPatente = new System.Windows.Forms.Label();
+            this.txtPatente = new System.Windows.Forms.TextBox();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblModelo
+            // 
+            this.lblModelo.AutoSize = true;
+            this.lblModelo.Font = new System.Drawing.Font("Gabriola", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModelo.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblModelo.Location = new System.Drawing.Point(126, 79);
+            this.lblModelo.Name = "lblModelo";
+            this.lblModelo.Size = new System.Drawing.Size(81, 45);
+            this.lblModelo.TabIndex = 1;
+            this.lblModelo.Text = "Modelo";
+            // 
+            // lblPatente
+            // 
+            this.lblPatente.AutoSize = true;
+            this.lblPatente.Font = new System.Drawing.Font("Gabriola", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPatente.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblPatente.Location = new System.Drawing.Point(126, -8);
+            this.lblPatente.Name = "lblPatente";
+            this.lblPatente.Size = new System.Drawing.Size(81, 45);
+            this.lblPatente.TabIndex = 2;
+            this.lblPatente.Text = "Patente";
+            // 
+            // txtPatente
+            // 
+            this.txtPatente.BackColor = System.Drawing.Color.Black;
+            this.txtPatente.Font = new System.Drawing.Font("Gabriola", 18F, System.Drawing.FontStyle.Bold);
+            this.txtPatente.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.txtPatente.Location = new System.Drawing.Point(114, 28);
+            this.txtPatente.Name = "txtPatente";
+            this.txtPatente.Size = new System.Drawing.Size(109, 48);
+            this.txtPatente.TabIndex = 3;
+            this.txtPatente.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.BackColor = System.Drawing.Color.Black;
+            this.txtModelo.Font = new System.Drawing.Font("Gabriola", 18F, System.Drawing.FontStyle.Bold);
+            this.txtModelo.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.txtModelo.Location = new System.Drawing.Point(114, 112);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(109, 48);
+            this.txtModelo.TabIndex = 4;
+            this.txtModelo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.BackColor = System.Drawing.Color.Black;
+            this.btnCrear.Font = new System.Drawing.Font("Gabriola", 18F, System.Drawing.FontStyle.Bold);
+            this.btnCrear.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.btnCrear.Location = new System.Drawing.Point(114, 180);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(109, 50);
+            this.btnCrear.TabIndex = 5;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = false;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // frmPickUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InfoText;
+            this.ClientSize = new System.Drawing.Size(365, 268);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.txtModelo);
+            this.Controls.Add(this.txtPatente);
+            this.Controls.Add(this.lblPatente);
+            this.Controls.Add(this.lblModelo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPickUp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Crear Pickup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblModelo;
+        private System.Windows.Forms.Label lblPatente;
+        private System.Windows.Forms.TextBox txtPatente;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.Button btnCrear;
+    }
+}
+
---
+++ Modelos_Examenes/2018_PP_Estacionamiento/PP_Estacionamiento_Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("PP_Estacionamiento_Console")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PP_Estacionamiento_Console")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("08a841e2-c26a-4e6e-ba17-1be617ef1c90")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
