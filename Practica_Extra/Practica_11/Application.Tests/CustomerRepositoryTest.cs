9293be5c1e53ba39a078ff4a4cd3db9d74c08f65
[FacuFalcone] <falcone.facu@gmail.com>
2021-06-06 23:25:35 -0300

    Project UnitTests

---
+++ Practica_Extra/Practica_11/Application.Tests/CustomerRepositoryTest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Application.Repositories;
+using Application.Models;
+
+namespace Application.Tests
+{
+    [TestClass]
+    public class CustomerRepositoryTest
+    {
+        /// <summary>
+        ///A test for Create
+        ///</summary>
+        [TestMethod()]
+        public void CreateSuccessfullyTest()
+        {
+            //Arrange
+            CustomerRepository repository = new CustomerRepository();
+
+            int millisecond = DateTime.Now.Millisecond;
+
+            Customer entityToCreate = new Customer()
+            {
+                LastName = "TestCustomerLastName" + millisecond,
+                Name = "TestCustomerName" + millisecond
+            };
+            //Notar que entityToCreate.Id es null en este momento
+
+
+            //Act
+            repository.Create(entityToCreate);
+
+            Customer entityToValidate = repository.GetById(entityToCreate.ID);
+
+            //Assert
+            Assert.IsNotNull(entityToValidate);
+            Assert.IsTrue(entityToValidate.ID > 0);
+        }
+
+        ///// <summary>
+        /////A test for GetAll
+        /////</summary>
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            //Arrange
+            CustomerRepository target = null; // TODO: Inicializar de manera apropiada
+            List<Customer> expected = null; // TODO: Inicializar de manera apropiada
+            List<Customer> actual;
+
+            //Act
+            actual = target.GetAll();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        ///// <summary>
+        /////A test for GetById
+        /////</summary>
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            //Arrange
+            CustomerRepository target = null; // TODO: Inicializar de manera apropiada
+            long entityId = 1;
+            Customer expected = null; // TODO: Inicializar de manera apropiada 
+            Customer actual;
+
+            //Act
+            actual = target.GetById(entityId);
+
+            //Assert
+            Assert.AreEqual(expected.LastName, actual.LastName);
+
+        }
+        ///// <summary>
+        /////A test for Remove
+        /////</summary>
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            //Arrange
+            CustomerRepository target = null; // TODO: Inicializar de manera apropiada 
+            Customer entity = null;// TODO: Inicializar de manera apropiada
+            entity.ID = 1;
+            target.Remove(entity);
+            Customer actual;
+
+            //Act
+
+            //Assert
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+
+        }
+        ///// <summary>
+        /////A test for Update
+        /////</summary>
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            //Arrange
+            CustomerRepository target = null; // TODO: Inicializar de manera apropiada 
+            Customer entity = null; // TODO: Inicializar de manera apropiada 
+
+            //Act
+            target.Update(entity);
+
+            //Assert
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+
+        }
+    }
+}
---
+++ Practica_Extra/Practica_11/Application.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Application.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Application.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e6bdcc16-ff86-4bf3-9d10-04ca7c0a50aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
