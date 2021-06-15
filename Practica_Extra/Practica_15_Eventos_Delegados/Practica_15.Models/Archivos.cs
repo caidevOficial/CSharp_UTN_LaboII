3a3a7d257c0920bf533bc5e32f6720af1319b6da
[FacuFalcone] <falcone.facu@gmail.com>
2021-06-15 20:35:33 -0300

    Practica_15

---
+++ Practica_Extra/Practica_15_Eventos_Delegados/Practica_15.Models/Archivos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Models {
+    public class Archivos {
+
+        public bool Guardar(Persona persona) {
+            try {
+                string path = $"{Environment.CurrentDirectory}\\File";
+                string fileName = "File.txt";
+                string message = $"Cajero cobro a {persona.ToString()} ${persona.MontoTotal}.";
+                if (!Directory.Exists(path)) {
+                    Directory.CreateDirectory(path);
+                }
+                using (StreamWriter writer = new StreamWriter($"{path}\\{fileName}")) {
+                    writer.WriteLine(message);
+                    return true;
+                }
+            } catch (Exception exe) {
+
+                throw new Exception(exe.Message);
+            }
+        }
+    }
+}
