b0aeb2571f89aee9394bda36b2bd324005531191
[FacuFalcone] <falcone.facu@gmail.com>
2021-06-15 21:07:24 -0300

    Practica_14 [Terminada]

--- Practica_Extra/Practica_14_SQL_Querys/Querys.sql
+++ Practica_Extra/Practica_14_SQL_Querys/Querys.sql
@@ -1,9 +1,9 @@
 -- Select Database
-using UTN;
+USE UTN;
 -- Drop tables
-drop table (Personas);
-drop table (Envios);
-drop table (Productos);
+DROP TABLE Proveedores;
+DROP TABLE Envios;
+DROP TABLE Productos;
 
 -- Create Tables
 CREATE TABLE Envios(Numero int NOT NULL, pNumero int NOT NULL, Cantidad int NOT NULL) ON [PRIMARY];
@@ -37,7 +37,7 @@ INSERT INTO Envios (Numero, pNumero, Cantidad) VALUES (102, 3, 300);
 SELECT * FROM productos ORDER BY pNombre;
 
 -- 2. Obtener los detalles completos de todos los proveedores de ‘Quilmes’.
-SELECT * FROM Proveedores WHERE Localidad='Quilmes';
+SELECT * AS De_Quilmes FROM Proveedores WHERE Localidad='Quilmes';
 
 -- 3. Obtener todos los envíos en los cuales la cantidad este entre 200 y 300 inclusive.
 SELECT * FROM Envios WHERE Cantidad BETWEEN 200 AND 300;
@@ -52,14 +52,34 @@ SELECT TOP 3 Numero FROM Envios;
 SELECT P.Nombre, Pr.pNombre from Proveedores AS P, Productos AS Pr, Envios AS E WHERE Pr.pNumero = E.pNumero AND P.Numero = E.Numero;
 
 -- 7. Indicar el monto (cantidad * precio) de todos los envíos.
+SELECT E.Numero as ENVIO, (E.Cantidad*P.Precio) AS PRECIO FROM Envios AS E, Productos AS P WHERE E.pNumero = P.pNumero;
 
 -- 8. Obtener la cantidad total del producto 1 enviado por el proveedor 102.
+SELECT SUM(Cantidad) AS Cantidad_Producto_1 FROM Envios WHERE pNumero = 1;
+
 -- 9. Obtener todos los números de los productos suministrados por algún proveedor de ‘Avellaneda’.
+SELECT pNumero AS De_Avellaneda FROM Envios AS E, Proveedores AS P WHERE E.Numero = P.Numero AND P.Localidad = 'Avellaneda';
+
 -- 10.Obtener los domicilios y localidades de los proveedores cuyos nombres contengan la letra ‘I’.
+SELECT Domicilio, Localidad FROM Proveedores AS P WHERE P.Nombre LIKE '%i%';
+
 -- 11.Agregar el producto numero 4, llamado ‘Chocolate’, de tamaño chico y con un precio de 25,35.
+INSERT INTO Productos (pNumero, pNombre, Precio, Tamanio) VALUES (4, 'Chocolate', 25.35, 'Chico');
+
 -- 12.Insertar un nuevo proveedor (únicamente con los campos obligatorios).
+INSERT INTO Proveedores (Numero, Nombre, Domicilio, Localidad) VALUES (103, 'Simpson', 'Falsa 123', 'Springfield');
+
 -- 13.Insertar un nuevo proveedor (107), donde el nombre y la localidad son ‘Rosales’ y ‘La Plata’.
+INSERT INTO Proveedores (Numero, Nombre, Domicilio, Localidad) VALUES (107, 'Rosales', 'Diagonal 8', 'La Plata');
+
 -- 14.Cambiar los precios de los productos de tamaño ‘grande’ a 97,50.
+UPDATE Productos SET Precio = 97.5 WHERE Tamanio = 'Grande';
+
 -- 15.Cambiar el tamaño de ‘Chico’ a ‘Mediano’ de todos los productos cuyas cantidades sean mayores a 300 inclusive.
+UPDATE Productos SET Tamanio = 'Mediano' WHERE Tamanio = 'Chico';
+
 -- 16.Eliminar el producto número 1.
+DELETE FROM Productos WHERE pNumero = 1;
+
 -- 17.Eliminar a todos los proveedores que no han enviado productos.
+DELETE FROM Proveedores WHERE NOT EXISTS (SELECT * FROM Envios AS E WHERE E.Numero = Proveedores.Numero);
