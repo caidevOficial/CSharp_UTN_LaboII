b6c6947ee1a62c7e0102de06fd7a75640839d3b0
[FacuFalcone] <falcone.facu@gmail.com>
2021-06-26 17:05:52 -0300

    Ejercicio_63 [Terminado]

---
+++ Ejercicios_Guia/Ejercicio_63/Ejercicio_63/frmEjer63.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace Ejercicio_63 {
+    partial class frmEjer63 {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEjer63));
+            this.lblHora = new System.Windows.Forms.Label();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lblHora
+            // 
+            this.lblHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.Location = new System.Drawing.Point(12, 9);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(389, 34);
+            this.lblHora.TabIndex = 0;
+            this.lblHora.Text = "Time";
+            this.lblHora.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rtbText
+            // 
+            this.rtbText.BackColor = System.Drawing.Color.Black;
+            this.rtbText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbText.ForeColor = System.Drawing.Color.OrangeRed;
+            this.rtbText.Location = new System.Drawing.Point(10, 46);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(391, 237);
+            this.rtbText.TabIndex = 1;
+            this.rtbText.Text = "";
+            // 
+            // frmEjer63
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(414, 293);
+            this.Controls.Add(this.rtbText);
+            this.Controls.Add(this.lblHora);
+            this.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmEjer63";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ejercicio 63";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmEjer63_FormClosing);
+            this.Load += new System.EventHandler(this.frmEjer63_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.RichTextBox rtbText;
+    }
+}
+
---
+++ Ejercicios_Guia/Ejercicio_63/Ejercicio_63/frmEjer63.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * MIT License
+ * 
+ * Copyright (c) 2021 [FacuFalcone]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Ejercicio_63 {
+    public partial class frmEjer63 : Form {
+
+        public delegate void DelegadoHora(string dato);
+        public event DelegadoHora myEvent;
+        public Thread myThread;
+
+        /// <summary>
+        /// Builds the form.
+        /// </summary>
+        public frmEjer63() {
+            InitializeComponent();
+            myThread = new Thread(IniciarHora);
+            myEvent += AsignarHora;
+        }
+
+        /// <summary>
+        /// Load Event Handler of the form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmEjer63_Load(object sender, EventArgs e) {
+            myThread.Start();
+        }
+
+        /// <summary>
+        /// Assign the time to the label.
+        /// </summary>
+        /// <param name="dato">Time in string formato to assign.</param>
+        private void AsignarHora(string dato) {
+            if (this.lblHora.InvokeRequired) {
+                DelegadoHora dh = new DelegadoHora(AsignarHora);
+                object[] objs = new object[] { dato };
+                this.Invoke(dh, objs);
+            } else {
+                this.lblHora.Text = dato;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the time.
+        /// </summary>
+        private void IniciarHora() {
+            for (; ; ) {
+                myEvent.Invoke(DateTime.Now.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Close event Handler of the form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmEjer63_FormClosing(object sender, FormClosingEventArgs e) {
+            if (myThread.IsAlive) {
+                myThread.Abort();
+            }
+        }
+    }
+}
