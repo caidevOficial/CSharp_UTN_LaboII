bfac1028dbe7392f28daaf384deeb93ca880d1d7
CaidevOficial <falcone.facu@gmail.com>
2021-07-12 21:49:30 -0300

    Array Colecciones - Ejercicios 26 A 30

---
+++ 1_Ejercicios_Guia/06_Array_Colecciones_Ej_26_30/Ejercicio_28/Ejercicio_28/WordsCounter.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * MIT License
+ * 
+ * Copyright (c) 2021 [FacuFalcone]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Ejercicio_28 {
+    public partial class ContadorDePalabras : Form {
+        public ContadorDePalabras() {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Counts the different words and shows in the app.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e) {
+            Dictionary<string, int> amountWords = new Dictionary<string, int>();
+            List<string> orderedTop = new List<string>();
+            string finalMessage = "";
+            string keyValue;
+            bool first = true;
+            int biggerNumber = int.MinValue;
+            if (!String.IsNullOrWhiteSpace(richTextWords.Text)) {
+                string words = richTextWords.Text;
+                string[] arrayWords;
+                words.Trim(); // deletes the white spaces at the end & beginning
+                words = words.ToLower(); // normalize the words.
+                words = words.Replace(".", ""); // deletes the dots
+                arrayWords = words.Split(' ');
+
+                // Counts Words
+                foreach (string word in arrayWords) {
+                    if (!amountWords.ContainsKey(word)) {
+                        amountWords.Add(word, 1);
+                    } else {
+                        amountWords[word] += 1;
+                    }
+                }
+
+                // Obtains the k,v pair and shows into the text box.
+                foreach (KeyValuePair<string, int> row in amountWords) {
+                    keyValue = $"{row.Value} - {row.Key}. \n";
+                    if (first || row.Value >= biggerNumber) {
+                        first = false;
+                        biggerNumber = row.Value;
+                        orderedTop.Insert(0, keyValue);
+                    } else {
+                        orderedTop.Add(keyValue);
+                    }
+                }
+
+                for (int i = 0; i < 3; i++) {
+                    finalMessage += orderedTop[i];
+                }
+                richTextShow.Text = "Top 3 palabras con mas apariciones:\n" + finalMessage;
+            }
+        }
+
+        private void ContadorDePalabras_FormClosing(object sender, FormClosingEventArgs e) {
+            if (MessageBox.Show("Do you want to quit this wonderful app?", "Choose wisely", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No) {
+                e.Cancel = true;
+            } else {
+                Dispose();
+            }
+        }
+    }
+}
---
+++ 1_Ejercicios_Guia/06_Array_Colecciones_Ej_26_30/Ejercicio_30/Entities/AutoF1.cs
@@ -0,0 +1,137 @@
+﻿/*
+ * MIT License
+ * 
+ * Copyright (c) 2021 [FacuFalcone]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using System;
+
+namespace Entities {
+    public sealed class AutoF1 {
+        private short cantidadCombustible;
+        private bool enCompetencia;
+        private string escuderia;
+        private short numero;
+        private short vueltasRestantes;
+
+        #region Builders
+
+        /// <summary>
+        /// Builds the Car.
+        /// </summary>
+        /// <param name="escuderia">Team of the car.</param>
+        /// <param name="numero">Number of the car.</param>
+        public AutoF1(string escuderia, short numero) {
+            this.escuderia = escuderia;
+            this.numero = numero;
+            this.CantidadCombustible = 0;
+            this.VueltasRestantes = 0;
+            this.EnCompetencia = false;
+        }
+
+        #endregion
+
+        #region Getters
+
+        /// <summary>
+        /// Get: The amount of fuel.
+        /// Set: The amount of fuel (positive number only).
+        /// </summary>
+        public short CantidadCombustible {
+            get => cantidadCombustible;
+            set {
+                if (value >= 0 && value < 101) {
+                    cantidadCombustible = value;
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get: The remaining laps.
+        /// Set: The remaining laps (positive number only).
+        /// </summary>
+        public short VueltasRestantes {
+            get => vueltasRestantes;
+            set {
+                if (value >= 0) {
+                    vueltasRestantes = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get: The competition state of the car.
+        /// Set: The competition state of the car.
+        /// </summary>
+        public bool EnCompetencia {
+            get => enCompetencia;
+            set => enCompetencia = value;
+        }
+
+        #endregion
+
+        #region Operators
+
+        /// <summary>
+        /// Compares if the number and team of both cars are the same.
+        /// </summary>
+        /// <param name="a1">First car to compare.</param>
+        /// <param name="a2">Second car to compare.</param>
+        /// <returns>True if both cars are the same, otherwise false.</returns>
+        public static bool operator ==(AutoF1 a1, AutoF1 a2) {
+            if (!(a1 is null) && !(a2 is null)) {
+                if ((a1.numero == a2.numero) && (String.Compare(a1.escuderia, a2.escuderia) == 0)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Compares if the number and team of both cars are different.
+        /// </summary>
+        /// <param name="a1">First car to compare.</param>
+        /// <param name="a2">Second car to compare.</param>
+        /// <returns>True if both cars are different, otherwise false.</returns>
+        public static bool operator !=(AutoF1 a1, AutoF1 a2) {
+            return !(a1 == a2);
+        }
+
+        #endregion
+
+        #region ShowData
+
+        /// <summary>
+        /// Shows the data of the car.
+        /// </summary>
+        /// <returns>The data as a string.</returns>
+        public string MostrarDatos() {
+            string stats = String.Format("Number: {0,2} | Team: {1,-6} | Comp: {2,-5} | Fuel: {3,3}% | Laps: {4,2}",
+                this.numero, this.escuderia, this.EnCompetencia, this.CantidadCombustible, this.VueltasRestantes);
+
+            return stats;
+        }
+
+        #endregion
+    }
+}
