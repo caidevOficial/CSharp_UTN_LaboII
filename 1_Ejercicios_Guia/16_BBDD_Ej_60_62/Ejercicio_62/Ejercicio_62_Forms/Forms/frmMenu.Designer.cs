a6376ed343fcf440d5fec5443f8e43f50febaac9
CaidevOficial <falcone.facu@gmail.com>
2021-07-12 21:53:08 -0300

    BBDD - Ejercicios 60 A 62

---
+++ 1_Ejercicios_Guia/16_BBDD_Ej_60_62/Ejercicio_62/Ejercicio_62_Forms/Forms/frmMenu.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace Ejercicio_55_Forms {
+    partial class frmMenu {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenu));
+            this.grpButtons = new System.Windows.Forms.GroupBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnFacturacionProvincial = new System.Windows.Forms.Button();
+            this.btnFacturacionLocal = new System.Windows.Forms.Button();
+            this.btnFacturacionTotal = new System.Windows.Forms.Button();
+            this.btnGenerarLlamada = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.grpButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpButtons
+            // 
+            this.grpButtons.Controls.Add(this.btnSalir);
+            this.grpButtons.Controls.Add(this.btnFacturacionProvincial);
+            this.grpButtons.Controls.Add(this.btnFacturacionLocal);
+            this.grpButtons.Controls.Add(this.btnFacturacionTotal);
+            this.grpButtons.Controls.Add(this.btnGenerarLlamada);
+            this.grpButtons.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.grpButtons.Location = new System.Drawing.Point(12, 12);
+            this.grpButtons.Name = "grpButtons";
+            this.grpButtons.Size = new System.Drawing.Size(313, 432);
+            this.grpButtons.TabIndex = 0;
+            this.grpButtons.TabStop = false;
+            this.grpButtons.Text = "Buttons";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Black;
+            this.btnSalir.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnSalir.Location = new System.Drawing.Point(77, 319);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(165, 57);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnFacturacionProvincial
+            // 
+            this.btnFacturacionProvincial.BackColor = System.Drawing.Color.Black;
+            this.btnFacturacionProvincial.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnFacturacionProvincial.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnFacturacionProvincial.Location = new System.Drawing.Point(77, 244);
+            this.btnFacturacionProvincial.Name = "btnFacturacionProvincial";
+            this.btnFacturacionProvincial.Size = new System.Drawing.Size(165, 57);
+            this.btnFacturacionProvincial.TabIndex = 3;
+            this.btnFacturacionProvincial.Text = "Facturacion Provincial";
+            this.btnFacturacionProvincial.UseVisualStyleBackColor = false;
+            this.btnFacturacionProvincial.Click += new System.EventHandler(this.btnFacturacionProvincial_Click);
+            // 
+            // btnFacturacionLocal
+            // 
+            this.btnFacturacionLocal.BackColor = System.Drawing.Color.Black;
+            this.btnFacturacionLocal.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnFacturacionLocal.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnFacturacionLocal.Location = new System.Drawing.Point(77, 169);
+            this.btnFacturacionLocal.Name = "btnFacturacionLocal";
+            this.btnFacturacionLocal.Size = new System.Drawing.Size(165, 57);
+            this.btnFacturacionLocal.TabIndex = 2;
+            this.btnFacturacionLocal.Text = "Facturacion Local";
+            this.btnFacturacionLocal.UseVisualStyleBackColor = false;
+            this.btnFacturacionLocal.Click += new System.EventHandler(this.btnFacturacionLocal_Click);
+            // 
+            // btnFacturacionTotal
+            // 
+            this.btnFacturacionTotal.BackColor = System.Drawing.Color.Black;
+            this.btnFacturacionTotal.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFacturacionTotal.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnFacturacionTotal.Location = new System.Drawing.Point(77, 94);
+            this.btnFacturacionTotal.Name = "btnFacturacionTotal";
+            this.btnFacturacionTotal.Size = new System.Drawing.Size(165, 57);
+            this.btnFacturacionTotal.TabIndex = 1;
+            this.btnFacturacionTotal.Text = "Facturacion Total";
+            this.btnFacturacionTotal.UseVisualStyleBackColor = false;
+            this.btnFacturacionTotal.Click += new System.EventHandler(this.btnFacturacionTotal_Click);
+            // 
+            // btnGenerarLlamada
+            // 
+            this.btnGenerarLlamada.BackColor = System.Drawing.Color.Black;
+            this.btnGenerarLlamada.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnGenerarLlamada.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnGenerarLlamada.Location = new System.Drawing.Point(77, 19);
+            this.btnGenerarLlamada.Name = "btnGenerarLlamada";
+            this.btnGenerarLlamada.Size = new System.Drawing.Size(165, 57);
+            this.btnGenerarLlamada.TabIndex = 0;
+            this.btnGenerarLlamada.Text = "Generar Llamada";
+            this.btnGenerarLlamada.UseVisualStyleBackColor = false;
+            this.btnGenerarLlamada.Click += new System.EventHandler(this.btnGenerarLlamada_Click);
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.ClientSize = new System.Drawing.Size(348, 450);
+            this.Controls.Add(this.grpButtons);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Central Telefonica";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMenu_FormClosing);
+            this.grpButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpButtons;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnFacturacionProvincial;
+        private System.Windows.Forms.Button btnFacturacionLocal;
+        private System.Windows.Forms.Button btnFacturacionTotal;
+        private System.Windows.Forms.Button btnGenerarLlamada;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
