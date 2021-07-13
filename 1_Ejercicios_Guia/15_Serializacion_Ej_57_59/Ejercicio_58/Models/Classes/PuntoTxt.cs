84b91702effdc6d64274c1834db1ffb7cde956e7
CaidevOficial <falcone.facu@gmail.com>
2021-07-12 21:52:51 -0300

    Serializacion - Ejercicios 57 A 59

---
+++ 1_Ejercicios_Guia/15_Serializacion_Ej_57_59/Ejercicio_58/Models/Classes/PuntoTxt.cs
@@ -0,0 +1,107 @@
+﻿/*
+ * MIT License
+ * 
+ * Copyright (c) 2021 [FacuFalcone]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using Exceptions;
+using Interface;
+using System;
+using System.IO;
+
+namespace Models {
+
+    public class PuntoTxt : Archivo, IArchivos<string> {
+
+        #region Methods
+
+        /// <summary>
+        /// Saves the file
+        /// </summary>
+        /// <param name="path">Path to save the file.</param>
+        /// <param name="objeto">Content of the file to save.</param>
+        /// <returns>True if can save the file, otherwise returns false.</returns>
+        public bool Guardar(string path, string objeto) {
+            if (this.ValidarArchivo(path, true)) {
+                using (StreamWriter sw = File.AppendText(path)) {
+                    sw.WriteLine(objeto);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Saves the file
+        /// </summary>
+        /// <param name="path">Path to save the file.</param>
+        /// <param name="objeto">Content of the file to save.</param>
+        /// <returns>True if can save the file, otherwise returns false.</returns>
+        public bool GuardarComo(string path, string objeto) {
+            return this.Guardar(path, objeto);
+        }
+
+        /// <summary>
+        /// Reads a file and get its content.
+        /// </summary>
+        /// <param name="path">Path of the file.</param>
+        /// <returns>The content of the file if can read it.</returns>
+        public string Leer(string path) {
+            string text = string.Empty;
+            if (this.ValidarArchivo(path, true)) {
+                using (StreamReader sr = new StreamReader(path)) {
+                    text = sr.ReadToEnd();
+                }
+            }
+
+            return text;
+        }
+
+        /// <summary>
+        /// It will validate that the file exist only if 'calidateExistence' is true
+        /// otherwise will throw an exception.
+        /// </summary>
+        /// <param name="path">Path to verify if the file exist.</param>
+        /// <param name="validateExistence">Boolean state that indicates fi the method need to validate the existence of the file.</param>
+        /// <returns>True if is a valid file, othewise returns false</returns>
+        protected override bool ValidarArchivo(string path, bool validateExistence) {
+            try {
+                if (base.ValidarArchivo(path, validateExistence)) {
+                    if (Path.GetExtension(path) == ".txt") {
+                        return true;
+                    } else {
+                        throw new ArchivoIncorrectoException("El archivo no es un txt.");
+                    }
+                }
+            } catch (FileNotFoundException fn) {
+                throw new ArchivoIncorrectoException("El archivo no es un txt.", fn);
+            } catch (ArchivoIncorrectoException fn) {
+                throw new ArchivoIncorrectoException("El archivo no es un txt.", fn);
+            }
+
+            return false;
+        }
+
+        #endregion
+
+    }
+}
---
+++ 1_Ejercicios_Guia/15_Serializacion_Ej_57_59/Ejercicio_59/CentralitaHerencia_Console/Test.cs
@@ -0,0 +1,81 @@
+﻿
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2021 [FacuFalcone]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using CentralitaHerencia;
+using System;
+
+namespace CentralitaHerencia_Console {
+    class Test {
+        static void Main(string[] args) {
+
+            #region Attributes
+
+            string centralitaPath = $"{Environment.CurrentDirectory}";
+            string centralitaFileName = "Bitacora";
+
+            #endregion
+
+            #region Instances
+
+            Centralita c = new Centralita("Fede Center");
+
+            Local l1 = new Local("Bernal", "Rosario", 33, (float)2.85);
+            Provincial l2 = new Provincial(29, "Morón", "Bernal", Provincial.Franja.Franja_01);
+            Local l3 = new Local("Lanús", "San Rafael", 55, 1.98f);
+            Provincial l4 = new Provincial(l2, Provincial.Franja.Franja_03);
+
+            #endregion
+
+            // Las llamadas se irán registrando en la Centralita.
+            // La centralita mostrará por pantalla todas las llamadas según las vaya registrando.
+            try {
+                c += l1;
+                c.Guardar(centralitaPath, centralitaFileName);
+                l1.Guardar(centralitaPath, l1);
+                c += l2;
+                c.Guardar(centralitaPath, centralitaFileName);
+                l2.Guardar(centralitaPath, l2);
+                c += l3;
+                c.Guardar(centralitaPath, centralitaFileName);
+                l3.Guardar(centralitaPath, l3);
+                c += l4;
+                c.Guardar(centralitaPath, centralitaFileName);
+                l4.Guardar(centralitaPath, l4);
+                c += l4; // Duplicado
+            } catch (CentralitaException ce) {
+                Console.WriteLine($"Error: {ce.Message} producido en {ce.NombreClase} al usar {ce.NombreMetodo}.");
+            } catch (FallaLogException fl) {
+                Console.WriteLine($"Error: {fl.Message} producido en {fl.NombreClase} al usar {fl.NombreMetodo}.");
+            }
+            c.OrdenarLlamadas();
+            Console.WriteLine(c.ToString());
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("_________________________________");
+            Console.WriteLine(c.Leer());
+            Console.ReadKey();
+        }
+    }
+}
