38e08362001042cac489f16400ae423c5a42b3d9
CaidevOficial <falcone.facu@gmail.com>
2021-07-29 17:43:05 -0300

    SP_2018_Patentes

--- 2_Modelos_Examenes/SP_2018_Patentes/Patentes.GUI/Program.cs
+++ 2_Modelos_Examenes/SP_2018_Patentes/Patentes.GUI/Program.cs
@@ -1,19 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace _20181122_SP
-{
-    static class Program
-    {
+namespace _20181122_SP {
+    internal static class Program {
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
-        static void Main()
-        {
+        private static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmPpal());
